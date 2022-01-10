a5ab0504b7804f44647d3a3f604d18a93db95f04
Scott McCormack <scott.mccormack@woodside.com.au>
2022-01-10 22:24:40 +0800

    Update to suit 0.5.0 compiler

--- browser/Greeter.sol
+++ browser/Greeter.sol
@@ -1,17 +1,18 @@
-pragma solidity ^0.4.11;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.16 <0.9.0;
 
 contract Greeter {
     /* Define variuable greeting of the type String */
-    string public yourName;
+    string yourName;
 
     /* This runs when the contract is executed */
-    function Greeter() public {
+    function greeter() public {
         yourName = "World";
     }
-    function set(string name) public {
+    function set(string memory name) public {
         yourName = name;
     }
-    function hello() constant public returns (string) {
+    function hello() public view returns (string memory) {
         return yourName;
     }
 }
